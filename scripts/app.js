3c9a91f327ed8264e6224b2f158f0e6e115e3f88
Daniil <vladimirskiy.d@gmail.com>
2025-02-05 22:35:01 +0300

    Загрузка проекта

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
 document.documentElement.setAttribute('data-theme', tg.colorScheme);
+
 tg.onEvent('themeChanged', () => {
     document.documentElement.setAttribute('data-theme', tg.colorScheme);
 }); 
